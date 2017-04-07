d9f026f1ca0429162e0c4afef5662b03c20a331e
Evan Shimizu <falindrith@gmail.com>
2017-04-07 14:09:40 -0400

    0.15.1

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -40,8 +40,8 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "AttributesInterface";
-    const char* const  versionString  = "0.15.0";
-    const int          versionNumber  = 0xf00;
+    const char* const  versionString  = "0.15.1";
+    const int          versionNumber  = 0xf01;
 }
 #endif
 
